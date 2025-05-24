8ee25a9cadb4c7f8772118646fb016207749ad36
Medan Abebe <medanabebe17@gmail.com>
2025-05-24 21:53:11 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dart\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Flatter\\flatter assignment3\\album_viewer" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dart\\flutter"
+  "PROJECT_DIR=D:\\Flatter\\flatter assignment3\\album_viewer"
+  "FLUTTER_ROOT=C:\\dart\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Flatter\\flatter assignment3\\album_viewer\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Flatter\\flatter assignment3\\album_viewer"
+  "FLUTTER_TARGET=D:\\Flatter\\flatter assignment3\\album_viewer\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Flatter\\flatter assignment3\\album_viewer\\.dart_tool\\package_config.json"
+)
